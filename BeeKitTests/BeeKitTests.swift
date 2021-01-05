dc57d06111cdfc0e7acc2638d0b5309e5a9628f5
haoshuai <haoshuai9291@163.com>
2021-01-05 09:24:17 +0800

    Initial Commit

---
+++ BeeKitTests/BeeKitTests.swift
@@ -0,0 +1,33 @@
+//
+//  BeeKitTests.swift
+//  BeeKitTests
+//
+//  Created by flqy on 2021/1/5.
+//
+
+import XCTest
+@testable import BeeKit
+
+class BeeKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
